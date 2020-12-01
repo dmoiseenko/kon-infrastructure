ed4f11191c3b5527f92a3da319e2076b8f2fc46c
Denis Moiseenko <dmoiseenko@localhost.localdomain>
2020-12-01 10:57:35 +0300

    Istio

--- environment/development/module/rbac/main.tf
+++ environment/development/module/rbac/main.tf
@@ -3,20 +3,3 @@ resource "gsuite_group_member" "group_gke_security" {
   email = var.group_gke_admin
   role  = "MEMBER"
 }
-
-# resource "kubectl_manifest" "test" {
-#   yaml_body = <<YAML
-#     kind: ClusterRoleBinding
-#     apiVersion: rbac.authorization.k8s.io/v1beta1
-#     metadata:
-#       name: admin
-#     roleRef:
-#       kind: ClusterRole
-#       name: admin
-#       apiGroup: rbac.authorization.k8s.io
-#     subjects:
-#     - kind: Group
-#       name: group-admin-prj-kon-app-d-b7ec@dmoiseenko.me
-#       namespace: flux-system
-#   YAML
-# }
